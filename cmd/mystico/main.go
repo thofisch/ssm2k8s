a35994546398a6c27563751309f729904ddfa109
Thomas Wittrup Fischer <thofisch@gmail.com>
2019-04-25 17:38:24 +0200

    add console logger

--- cmd/mystico/main.go
+++ cmd/mystico/main.go
@@ -1,94 +1,48 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"github.com/thofisch/ssm2k8s"
-	"github.com/thofisch/ssm2k8s/k8s"
-	"os"
+	"github.com/thofisch/ssm2k8s/aws"
+	"github.com/thofisch/ssm2k8s/internal/logging"
 	"strings"
-
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
-	stdlog "log"
+	"time"
 )
 
-type keyValuePairs map[string]string
-
-func (kvp *keyValuePairs) String() string {
-	return fmt.Sprintf("Value: %#v\n", *kvp)
-}
-
-func (kvp *keyValuePairs) Set(value string) error {
-	split := strings.Split(value, "=")
-	if len(split) != 2 {
-		return fmt.Errorf("'%s' is unaccepted", value)
-	}
-
-	map[string]string(*kvp)[split[0]] = split[1]
-
-	return nil
-}
-
 func main() {
-	var keyValuePairs keyValuePairs = make(keyValuePairs)
+	logger := logging.NewConsoleLogger()
 
-	flag.Var(&keyValuePairs, "list1", "some description")
-	flag.Parse()
+	//var keyValuePairs keyValuePairs = make(keyValuePairs)
+	//
+	//flag.NewFlagSet()
+	//
+	//flag.Var(&keyValuePairs, "list1", "some description")
+	//port := flag.Int("port", 8088, "Service Port Number")
+	//flag.Parse()
+	//
+	//fmt.Printf("%#v\n", keyValuePairs)
+	//fmt.Printf("%d\n", port)
 
-	fmt.Printf("%#v\n", keyValuePairs)
 
-	logger := NewLogger("mysticod", "v0.1")
 
-	level.Error(logger).Log("msg", "TCP Failure", "error", nil)
 
-	port := flag.Int("port", 8088, "Service Port Number")
-	flag.Parse()
 
-	level.Info(logger).Log("msg", "Customer Service listening", "port", *port)
 
-	ssm2k8s.CreateParameters()
-
-	k8s.PrintSecret()
 }
 
-func NewLogger(serviceName string, version string) (logger log.Logger) {
-	logger = log.NewJSONLogger(log.NewSyncWriter(os.Stdout))
-	logger = level.NewFilter(logger, level.AllowAll())
-	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
-	logger = log.With(logger, "caller", log.DefaultCaller)
-	logger = log.With(logger, "svc", serviceName)
-	logger = log.With(logger, "version", version)
-	stdlog.SetOutput(log.NewStdlibAdapter(logger))
-	return
-}
-
-
-//func CreateParameters() {
-//	accountId, err := GetAccountId()
-//	if err != nil {
-//		panic(err)
+//
+//type keyValuePairs map[string]string
+//
+//func (kvp *keyValuePairs) String() string {
+//	return fmt.Sprintf("Value: %#v\n", *kvp)
+//}
+//
+//func (kvp *keyValuePairs) Set(value string) error {
+//	split := strings.Split(value, "=")
+//	if len(split) != 2 {
+//		return fmt.Errorf("'%s' is unaccepted", value)
 //	}
 //
-//	path := "/p-project/"
-//	fmt.Printf("Creating AWS SystemManager Parameters in \033[33m%s\033[0m from account \033[33m%s\033[0m \n", path, accountId)
+//	map[string]string(*kvp)[split[0]] = split[1]
 //
-//	newParameters := []struct {
-//		environment string
-//		application string
-//		key         string
-//		value       string
-//		secret      bool
-//	}{
-//		{"prod", "default", "kafka_bootstrap_servers", "pkc-l9pve.eu-west-1.aws.confluent.cloud:9092", true},
-//		{"prod", "default", "kafka_sasl_username", "2QIFZBFI32KT4I5Q", true},
-//		{"prod", "default", "kafka_sasl_password", "1gOjGYMZrDqHQe3mlYpytZP5ScnQGWkJnwUimIX8KkCK+4GHfDOOl+DnBjOMrFre", true},
-//	}
-//
-//	for _, v := range newParameters {
-//		name := parameterName{"p-project", v.environment, v.application, v.key}
-//
-//		fmt.Printf("%s\n", &name)
-//	}
-//
+//	return nil
 //}
--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -1,8 +1,10 @@
 package logging
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"os"
+	"strings"
 )
 
 type Logger interface {
@@ -23,13 +25,61 @@ type logrusLogger struct {
 
 func NewLogger() Logger {
 	logger := logrus.New()
-	logger.SetLevel(logrus.DebugLevel)
+	level := logrus.InfoLevel
+	if _, ok := os.LookupEnv("DEBUG"); ok {
+		level = logrus.DebugLevel
+	}
+	logger.SetLevel(level)
 	logger.SetOutput(os.Stdout)
 	//logger.SetFormatter(&logrus.JSONFormatter{})
 
 	return &logrusLogger{
 		logger: logger,
 	}
+}
+
+func NewConsoleLogger() Logger {
+	logger := logrus.New()
+	level := logrus.InfoLevel
+	timestamp := false
+	if _, ok := os.LookupEnv("DEBUG"); ok {
+		level = logrus.DebugLevel
+		timestamp = true
+	}
+	logger.SetLevel(level)
+	logger.SetOutput(os.Stdout)
+	logger.SetFormatter(&consoleFormatter{timestamp})
+
+	return &logrusLogger{
+		logger: logger,
+	}
+}
+
+type consoleFormatter struct {
+	timestamp bool
+}
+
+var colorMap = map[logrus.Level]string{
+	logrus.TraceLevel: "90",
+	logrus.DebugLevel: "90",
+	logrus.InfoLevel:  "39",
+	logrus.WarnLevel:  "33",
+	logrus.ErrorLevel: "31",
+	logrus.FatalLevel: "31",
+	logrus.PanicLevel: "31",
+}
+
+func (f *consoleFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	sb := &strings.Builder{}
+	sb.WriteString(fmt.Sprintf("\033[%sm", colorMap[entry.Level]))
+
+	if f.timestamp {
+		sb.WriteString(fmt.Sprintf("[%s] ", entry.Time.Format("2006-01-02 03:04:05")))
+	}
+	sb.WriteString(entry.Message)
+	sb.WriteString("\033[0m\n")
+
+	return []byte(sb.String()), nil
 }
 
 //func NewContextLogger(c logrus.Fields) func(f logrus.Fields) *logrus.Entry {
